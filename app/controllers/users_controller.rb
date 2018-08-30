a1981c256a4d54906651b1ae8728d4b564dec095
Josh Carroccia <agr.carroccia@gmail.com>
2018-08-30 18:32:54 -0400

    added the ability to create a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,28 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
   def index
+    @user = User.all
   end
 
   def new
+    @user = User.new
   end
-
   def show
   end
 
-  def edit
+  def create
+    @user = User.new(user_params)
+
+    @user.save
+    redirect_to root_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :username, :email, :password_digest)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/new'
-  get 'users/show'
-  get 'users/edit'
+  resources :users
+  root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
