483fe06fd4f1b076577c5af324e0431572c1b3ad
Josh Carroccia <agr.carroccia@gmail.com>
2018-08-31 10:43:42 -0400

    added in update and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,22 @@ class UsersController < ApplicationController
     redirect_to root_path
   end
 
+  def update
+    @user = User.find(params[:id])
+  
+    if @user.update(book_params)
+      redirect_to@user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+   
+    redirect_to user_path(@user)
+  end
   private
   def user_params
     params.require(:user).permit(:name, :username, :email, :password)
