7fa7d0d3e2d950c731190678129f479080939952
lstravers <stravers222@gmail.com>
2018-09-01 14:25:06 -0400

    added then removed css code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -33,7 +33,7 @@ class CommentsController < ApplicationController
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
-    redirect_to root_path
+    redirect_to 'posts#index'
   end
  
   private
