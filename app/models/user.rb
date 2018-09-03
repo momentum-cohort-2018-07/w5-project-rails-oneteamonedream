3116a0fdafc29db74f72bedc6266f772d9164c82
Stephen Patterson <swpat23@gmail.com>
2018-09-03 14:46:37 -0400

    added validation for user fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   has_many :comments
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  validates :name, presence: true, length: { minimum: 5 }
+  validates :username, presence: true, length: { minimum: 5 }
+  validates :password, presence: true, length: { minimum: 5 }
+  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+
 end
